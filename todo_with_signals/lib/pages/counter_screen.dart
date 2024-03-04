04b229bf828c18334fea05e8b79f8a0370ba0347
alessiogianello <giane199@gmail.com>
2024-03-04 10:55:46 +0100

    aggiunte dependencies

--- todo_with_signals/lib/pages/counter_screen.dart
+++ todo_with_signals/lib/pages/counter_screen.dart
@@ -46,7 +46,7 @@ class _CounterPageState extends State<CounterPage> {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the CounterPage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
--- todo_with_signals/lib/pages/home_screen.dart
+++ todo_with_signals/lib/pages/home_screen.dart
@@ -1,16 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'counter_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: ListView(
-        children: [],
-      ),
-    );
+        appBar: AppBar(title: Text("home")),
+        body: ListView(children: [
+          _NavItem(label: "Counter", screen: CounterPage(title: "counter"))
+        ]));
   }
 }
 
@@ -20,6 +20,11 @@ class _NavItem extends StatelessWidget {
   _NavItem({required this.label, required this.screen});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => screen));
+        },
+        child: Text(label));
   }
 }
